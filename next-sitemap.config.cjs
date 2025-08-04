baa9f3b64f58809522125d2458481e40203c791d
ChannabasavaH <channabasavahalemani49@gmail.com>
2025-08-04 17:31:24 +0530

    fixed next-sitemap error

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://brandandbrandz.com',
+  generateRobotsTxt: true,
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: [],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+  additionalPaths: async (config) => {
+    return [
+      await config.transform(config, '/about'),
+      await config.transform(config, '/service'),
+      await config.transform(config, '/team'),
+      await config.transform(config, '/client'),
+      await config.transform(config, '/contact'),
+    ];
+  },
+};
